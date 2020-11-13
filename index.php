836a1593e35b8407c91c7f409b13bc0d5480da32
Louka-Fauvel <73880235+Louka-Fauvel@users.noreply.github.com>
2020-11-13 12:16:49 +0100

    13/11/2020

--- index.php
+++ index.php
@@ -1,11 +1,9 @@
-<?php include "Page_d_accueil.php"; ?>
-<p></p>
-<?php include "A_propos.php"; ?>
-<p></p>
-<?php include "Compétences.php"; ?>
-<p></p>
-<?php include "Expérience.php"; ?>
-<p></p>
-<?php include "Formation.php"; ?>
-<p></p>
-<?php include "Contact.php"; ?>
+<html>
+    <head>
+        <meta charset="utf-8">
+    </head>
+    <body>
+    <h1>site-personnel-fl.local</h1>
+    <p>Vous consultez cette page depuis l'adresse <strong><?= $_SERVER['REMOTE_ADDR'] ?></strong></p>
+    </body>
+</html>
