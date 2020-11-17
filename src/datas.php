1525efbb8e58f221e74053a30498670f1e321f91
Louka-Fauvel <73880235+Louka-Fauvel@users.noreply.github.com>
2020-11-17 08:29:09 +0100

    17/11/2020

--- src/datas.php
+++ src/datas.php
@@ -1,2 +1,64 @@
 <?php
-$menu=['A propos','Compétences','Expérience','Formation','Contact'];
+$menu=['A_propos','Compétences','Expérience','Formation','Contact'];
+
+$a_propos=['titre'=>'<p class="titre_page">Louka Fauvel</p>','accroche'=>'<p class="accroche_page_A_propos">Sérieux et collectif</p>','texte'=>'<p class="texte_page_A_propos">Je suis élève en première année de BTS Service informatique aux organisations
+au pôle SupAvenir Sainte Ursule de Caen en Normandie. J\'aime beaucoup voyager
+et découvrir de nouvelles traditions culturelles. J\'ai eu la chance de participer
+J\'ai eu la chance de participer à deux échanges Erasmus avec la Suède et l\'Espagne
+en 2019. Je fais du dessin à l\'Esam de Caen et je pratique le théâtre depuis l\'âge
+de 7 ans. Je suis un élève sérieux qui aime travailler en équipe sur de nouveaux
+projets.</p>'];
+
+$competences=[['type'=>'Informatique et technique','competences'=> [
+                                                                    ['title'=>'A l\'aise ++','niveau'=>100,'items'=>['Adobe Photoshop','Adobe Premiere']],
+                                                                    ['title'=>'A l\'aise +','niveau'=>60,'items'=>['Labview','Arduino','Html/css','PacketTracer']],
+                                                                    ['title'=>'En cours de pratique','niveau'=>25,'items'=>['Apache','Python']]
+                                                                  ]
+             ],
+             ['type'=>'Langues','competences'=> [
+                                                  ['title'=>'A l\'aise ++','niveau'=>100,'items'=>['']],
+                                                  ['title'=>'A l\'aise +','niveau'=>60,'items'=>['Anglais']],
+                                                  ['title'=>'En cours de pratique','niveau'=>25,'items'=>['Espagnol']]
+                                                ]
+            ]];
+
+$experiences=[
+              ['image'=>'<img class="image_page_table" src="image/Erasmus.png">','contexte'=>'<b>ERASMUS +</b> <br>Institut Lemonnier <br>Octobre 2019 <br>1 semaine <br>Falkenberg, Suède','texte'=>'Concevoir et construire ensemble un véritable véhicule à énergie innovante.
+                <br>Travailler en pluridisciplinarité entre mécanique et mathématiques.
+                <br>Voyager jusqu’en Suède pour rejoindre les autres élèves en mobilité professionnelle.
+                <br>Contribuer à un « congrès de spécialistes » sur ce sujet pour la première fois.
+                <p>Langue de travail : Anglais</p>'],
+              ['image'=>'<img class="image_page_table" src="image/Erasmus.png">','contexte'=>'<b>ERASMUS +</b> <br>Institut Lemonnier <br>Mars 2019 <br>1 semaine <br>Caen, Normandie','texte'=>'Concevoir et construire ensemble un véritable véhicule à énergie innovante.
+                <br>Travailler en pluridisciplinarité entre mécanique et mathématiques.
+                <br>Accueillir des élèves venant d\'Espagne et de Suède.
+                <br>Rendre compte du projet en réalisant un film.
+                <p>Outils : Adobe Premiere
+                <br>Langue de travail : Anglais</p>'],
+              ['image'=>'<img class="image_page_table" src="image/Corlet.png">','contexte'=>'<b>Corlet</b> <br>Agence de communication <br>Janvier 2017 <br>1 semaine <br>Caen, Normandie','texte'=>'Observer le travail d\'un graphiste.
+                <br>Utiliser les outils du graphiste pour créer des images.
+                <p>Outils : Adobe Photoshop</p>']
+            ];
+
+$formations=[
+              ['image'=>'<img class="image_page_table" src="image/SupAvenir.png">','contexte'=>'<b>BTS SIO 1ère année</b> <br>SupAvenir Sainte-Ursule <br>2020/2021 <br>Caen, Normandie','texte'=>'Conception et administration de bases de données, conception Web,
+                <br>génie logiciel, cybersécurité et configuration de réseaux, architecture informatique.
+                <p>Langages et outils : HTML/CSS, PHP, Python, SQL
+                <br>Concepts : Algorithmique, POO, méthode Merise</p></td>'],
+              ['image'=>'<img class="image_page_table" src="image/Lemonnier.jpg">','contexte'=>'<b>Terminale STI2D SIN, section Euro</b> <br>Institut Lemonnier <br>2019/2020 <br>Caen, Normandie','texte'=>'Compétences étendues dans les 3 domaines «Matière, Energie, Information».
+                <br>Intégration les démarches d’éco-conception et de développement durable afin de répondre aux enjeux de notre société.
+                <br>Systèmes d’information et numérique : Analyse et création de solutions techniques.
+                <br>Traitement des flux d’information ( voix, données, images) dans les systèmes pluritechniques actuels.
+                <br>Gestion locale et à distance de l’information.
+                <p>Langages et outils : Arduino, Labview</p>'],
+              ['image'=>'<img class="image_page_table" src="image/Lemonnier.jpg">','contexte'=>'<b>Première STI2D SIN, section Euro</b> <br>Institut Lemonnier <br>2018/2019 <br>Caen, Normandie','texte'=>'Compétences étendues dans les 3 domaines «Matière, Energie, Information».
+                <br>Intégration les démarches d’éco-conception et de développement durable afin de répondre aux enjeux de notre société.
+                <br>Systèmes d’information et numérique : Analyse et création de solutions techniques.
+                <br>Traitement des flux d’information ( voix, données, images) dans les systèmes pluritechniques actuels.
+                <br>Gestion locale et à distance de l’information.
+                <p>Langages et outils : Arduino, Labview</p>'],
+              ['image'=>'<img class="image_page_table" src="image/Dumont.jpg">','contexte'=>'<b>Seconde générale</b> <br>Lycée Dumont d\'Urville <br>2017/2018 <br>Caen, Normandie','texte'=>'Option Sciences de l\'ingénieur']
+
+
+            ];
+
+ ?>
